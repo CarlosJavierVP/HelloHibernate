514539346957d66dba60f16a3aef9e2a83d46004
CarlosJavierValenzue <carlos.valenzuela.j92@gmail.com>
2024-10-17 13:27:12 +0200

    Commit conexión hibernate(session), consultas y guardar en la base de datos (persist)

--- src/main/java/hib/Main.java
+++ src/main/java/hib/Main.java
@@ -1,7 +1,44 @@
 package hib;
 
+import hib.model.Game;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
 public class Main {
     public static void main(String[] args) {
+
+
+        //Conexión a través de hibernate
+        SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+
+
+        try(Session session = sessionFactory.openSession()){
+            Query<Game> query = session.createQuery("select g from Game g", Game.class);
+
+            Query<String> query2 = session.createQuery("select g.title from Game g", String.class);
+
+            Query<String> query3 = session.createQuery("select distinct g.platform from Game g", String.class);
+
+            var resultado = query.list();
+            //query.getResultList();
+
+            resultado.forEach(System.out::println);
+
+
+            //las operaciones de escritura se hacen en el marco de una transacción
+            session.beginTransaction();
+            Game g = new Game();
+            g.setTitle("BG3");
+            g.setDescription("descripción");
+            g.setYear(2023);
+            g.setPlatform("PC");
+            g.setUserId(1);
+
+            session.persist(g);
+            session.getTransaction().commit();
+        }
 
     }
 }
--- src/main/java/hib/model/Game.java
+++ src/main/java/hib/model/Game.java
@@ -9,25 +9,20 @@ import java.util.Objects;
 @Data
 @Entity
 @Table(name = "games", schema = "ad")
-public class GamesEntity implements Serializable {
+public class Game implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
-    @Column(name = "id")
     private int id;
-    @Basic
-    @Column(name = "title")
+
     private String title;
-    @Basic
-    @Column(name = "platform")
+
     private String platform;
-    @Basic
-    @Column(name = "year")
+
     private Integer year;
-    @Basic
-    @Column(name = "description")
+
     private String description;
-    @Basic
-    @Column(name = "user_id")
+
+    @Column(name="user_id")
     private Integer userId;
 
 
@@ -36,7 +31,7 @@ public class GamesEntity implements Serializable {
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        GamesEntity that = (GamesEntity) o;
+        Game that = (Game) o;
         return id == that.id && Objects.equals(title, that.title) && Objects.equals(platform, that.platform) && Objects.equals(year, that.year) && Objects.equals(description, that.description) && Objects.equals(userId, that.userId);
     }
 
